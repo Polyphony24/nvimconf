8f486aea4a6f02a5cc8d113e8cbe7e74887e83f7
Malcolm Boyd <malcolmboyd24@gmail.com>
2024-09-13 01:34:37 -0300

    updated config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,63 +3,73 @@ return {
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
-		'williamboman/mason-lspconfig.nvim',
+		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require('mason-lspconfig').setup({
-				ensure_installed = { 'lua_ls', 'clangd', 'jdtls', 'ruff_lsp', 'pyright', 'rust_analyzer', 'taplo', 'gopls','html'}
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"jdtls",
+					"ruff_lsp",
+					"pyright",
+					"rust_analyzer",
+					"taplo",
+					"gopls",
+					"html",
+				},
 			})
-		end
+		end,
 	},
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require('lspconfig')
-			lspconfig.lua_ls.setup { capabilities = capabilities }
-			lspconfig.ruff.setup {
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.ruff.setup({
 				capabilities = capabilities,
-				filetypes = "python"
-			}
-			lspconfig.clangd.setup {}
-			lspconfig.jdtls.setup {}
-			lspconfig.rust_analyzer.setup {}
-			lspconfig.taplo.setup {}
-			lspconfig.gopls.setup {}
-			lspconfig.ts_ls.setup {}
+				filetypes = "python",
+			})
+			lspconfig.clangd.setup({})
+			lspconfig.jdtls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.taplo.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.ts_ls.setup({})
 
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
 					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 					local opts = { buffer = ev.buf }
-					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-					vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-					vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-					vim.keymap.set('n', '<leader>F', function()
-						vim.lsp.buf.format { async = true }
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<leader>F", function()
+						vim.lsp.buf.format({ async = true })
 					end, opts)
 				end,
 			})
-		end
+		end,
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
-			require('telescope').setup({
+			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
-						require('telescope.themes').get_dropdown {}
-					}
-				}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
-			require('telescope').load_extension('ui-select')
-		end
-	}
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,14 +1,19 @@
 return {
-	'nvimtools/none-ls.nvim',
+	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.completion.spell,
-				--require("none-ls.diagnostics.eslint"),
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.completion.luasnip,
+				--null_ls.builtins.diagnostics.cspell,
+				--null_ls.builtins.code_actions.cspell,
+				--null_ls.builtins.completion.spell,
+				--null_ls.builtins.diagnostics.eslint_d,
+				--null_ls.builtins.code_actions.eslint_d,
 			},
 		})
-	end
+	end,
 }
